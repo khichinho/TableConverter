9fbdc33fcd08f1cd761233c1912032eebb8e488e
Sanket Hire <sankethire98@gmail.com>
2018-05-20 20:07:41 +0530

    Update image_to_text.py

--- image_to_text.py
+++ image_to_text.py
@@ -3,22 +3,7 @@ from PIL import Image
 import pytesseract
 from wand.image import Image as wi 
 
-'''pdf = wi(filename = "b.pdf",resolution = 300)
-pdfImage = pdf.convert('jpeg')
-
-imageBlobs = []
-for img in pdfImage.sequence:
-	imgPage = wi(image = img)
-	imageBlobs.append(imgPage.make_blob('jpeg'))
-
-recognised_text = []
-
-for imgBlob in imageBlobs:
-	im = Image.open(io.BytesIO(imgBlob))
-	text = pytesseract.image_to_string(im,lang = 'eng')
-	recognised_text.append(text)
-
-print(recognised_text)'''	
+	
 im = Image.open("a.jpg")
 text = pytesseract.image_to_string(im,lang = 'eng')
 
