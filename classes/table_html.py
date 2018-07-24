69c15e4843af1a0793a191bb4f73815973c4c866
Yash Malviya <yashmalviya98@gmail.com>
2018-07-24 16:17:17 +0530

    Classes of image table and text defined

--- classes/table_html.py
+++ classes/table_html.py
@@ -1,3 +1,11 @@
 # Class of html table
 
-class table_html
+import cv2 as cv
+
+class Table_html:
+
+    def __init__(self, text_part, structure_part):
+        struct_binary = cv.adaptiveThreshold(structure_part,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
+        inv_struct_binary = ~struct_binary
+        _, contours, hierarchy= cv.findContours(struct_binary,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+        
--- classes/text.py
+++ classes/text.py
@@ -13,13 +13,20 @@ class Text:
         image = types.Image(content = content)
 
         response = client.text_detection(image = image)
-        texts = response.text_annotations
+        self.texts = response.text_annotations
+        self.max_size_bndpoly = Text.max_size_bounding_poly(self.texts)
 
-        text_list = texts[0].split('\n')
+    @staticmethod
+    def max_size_bounding_poly(response_list):
+        max_x = 0
+        max_y = 0
+        # assumed text size is lesser than width and height of table in pixels
+        for text_object in response_list:
+            bp_x = text_object.bounding_poly.vertices[3].x-text_object.bounding_poly.vertices[0].x
+            bp_y = text_object.bounding_poly.vertices[3].y-text_object.bounding_poly.vertices[0].y
+            if bp_x>max_x:
+                max_x = bp_x
+            if bp_y>max_y:
+                max_y = bp_y
         
-
-class Phrase:
-
-    def __init__(self, text, bnd_poly):
-        self.text = text
-        self.bnd_poly = bnd_poly
+        return (max_x, max_y)
