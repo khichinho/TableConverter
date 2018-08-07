b71b2a754fbcaf23d87eca712a91f691e8988c18
Yash Malviya <you@example.com>
2018-08-07 18:03:54 +0530

    text analysis, center of mass

--- classes/table_image.py
+++ classes/table_image.py
@@ -10,10 +10,14 @@ class Table_image:
     def __init__(self, image_file_name):
         original_image = cv.imread(image_file_name)
         original_binary = Table_image.convert_to_binary(original_image)
-        text_of_image = text.Text(image_file_name)
+        text_from_image = text.Text(image_file_name)
         structure = Table_image.extract_structure(
-            original_binary, text_of_image.max_size_bndpoly[0],
+            original_binary, text_from_image.max_size_bndpoly[0],
             text_of_image.max_size_bndpoly[1])
+        # could possible change coefficient_of_margin if some images detected
+        # wrongly
+        _, _, _ = Table_image.get_intersections(structure[0], structure[1])
+        
 
 
     @staticmethod
@@ -23,9 +27,7 @@ class Table_image:
             gray, 255, 0, cv.THRESH_BINARY, 15, -2)
         return binary
 
-        
-
-    @staticmethod 
+    @staticmethod
     def extract_structure(image, horizontal, vertical):
         horizontal_line_matrix = cv.getStructuringElement(
             cv.MORPH_RECT, (horizontal, 1))
@@ -39,4 +41,26 @@ class Table_image:
             image, cv.MORPH_OPEN, vertical_line_matrix)
         vertical_structure = cv.morphologyEx(
             vertical_structure, cv.MORPH_CLOSE, vertical_line_matrix)
-        
+        return (horizontal_structure, vertical_structure)
+
+    @staticmethod
+    def get_intersections(
+            horizontal_structure, vertical_structure):
+        intersections = cv.bitwise_and(
+            horizontal_structure, vertical_structure)
+        intersections_contours = cv.findContours(
+            intersections, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        row_cordinate_list = []
+        column_cordinate_list = []
+        for cntr in intersections_contours:
+            x, y, w, h = cv.boundingRect(cntr)
+            M = cv.moments(cntr)
+            cx = int(M['m10']/M['m00'])
+            cy = int(M['m01']/M['m00'])
+            for i in row_cordinate_list:
+                if not cx-(1/2)*w < i < cx+(1/2)*w:
+                    row_cordinate_list.append(cx)
+            for j in column_cordinate_list:
+                if not cy-(1/2)*h < i < cy+(1/2)*h:
+                    column_cordinate_list.append(cy)
+
--- classes/text.py
+++ classes/text.py
@@ -4,29 +4,39 @@ import io
 from google.cloud import vision
 from google.cloud.vision import types
 
+
 class Text:
 
     def __init__(self, image_file_name):
         with io.open(image_file_name, 'rb') as image_file:
             content = image_file.read()
         client = vision.ImageAnnotatorClient()
-        image = types.Image(content = content)
+        image = types.Image(content=content)
 
-        response = client.text_detection(image = image)
+        response = client.text_detection(image=image)
         self.texts = response.text_annotations
-        self.max_size_bndpoly = Text.max_size_bounding_poly(self.texts)
+        self.max_size_bndpoly, self.avg_size_bndpoly =
+        Text.max_size_bounding_poly(self.texts)
 
     @staticmethod
-    def max_size_bounding_poly(response_list):
+    def get_bounding_poly_attributes(response_list):
+        sum_x = 0
+        sum_y = 0
         max_x = 0
         max_y = 0
         # assumed text size is lesser than width and height of table in pixels
         for text_object in response_list:
-            bp_x = text_object.bounding_poly.vertices[3].x-text_object.bounding_poly.vertices[0].x
-            bp_y = text_object.bounding_poly.vertices[3].y-text_object.bounding_poly.vertices[0].y
-            if bp_x>max_x:
+            bp_x =
+            text_object.bounding_poly.vertices[3].x -
+            text_object.bounding_poly.vertices[0].x
+            bp_y =
+            text_object.bounding_poly.vertices[3].y -
+            text_object.bounding_poly.vertices[0].y
+            if bp_x > max_x:
                 max_x = bp_x
-            if bp_y>max_y:
+            if bp_y > max_y:
                 max_y = bp_y
-        
-        return (max_x, max_y)
+            sum_x += bp_x
+            sum_y += bp_y
+        return (max_x, max_y),
+        (sum_x/len(response_list), sum_y/len(response_list))
